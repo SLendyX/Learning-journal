f9137ebc2f1c35cae2445bdfca0fbcc99af2cdf7
SLendyX <slendyx2002@gmail.com>
2024-07-04 19:45:56 +0300

    main requirements done, only stretch goals remain

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,57 @@ import { articleArray } from "./article_data.js";
     <p></p>
 </article>
 */
+const articleBodyEl = document.getElementById("article-body");
+
 
 function renderArticles(articleArray){
     let articles=""
     articleArray.forEach(element => {
         articles+=`
+        <article class="blog-item">
+        <a href="${element.link}"><img src="/images/blog${element.image}-image.png"></a>
+            <time>${element.date}</time>
+            <h1>${element.title}</h1>
+            <p>${element.content}</p>
+        </article>
+`});
 
-<article>
-<a href="#"><img src="/images/blog${element.image}-image.png"></a>
-    <time>${element.date}</time>
-    <h1>${element.title}</h1>
-    <p>${element.content}</p>
-</article>
+    articleBodyEl.innerHTML+=articles
 
-`
-    });
-    document.getElementById("article-body").innerHTML+=articles
+    let i=0;
+    const blogItems = document.getElementsByClassName("blog-item");
+    for(let element of blogItems){
+        if(i > 5){
+            element.classList.toggle("hidden")
+            element.classList.toggle("blog-item-hidden")
+        }
+        i++;
+
+        console.log(element.className.split(" ")[1] === "hidden")
+    }
+    
+
+    document.getElementById("main-container").appendChild(createViewMoreBtn(blogItems))
+
 }
+
+function createViewMoreBtn(blogItems){
+    const btn = document.createElement("button");
+    btn.textContent = "View More"
+    btn.className = "view-more"
+    btn.addEventListener("click", e => {
+        for(let element of blogItems){
+            if(element.className.split(" ")[1] === "hidden"){
+                element.classList.toggle("hidden")
+                element.classList.add('show')
+            }
+        }
+        btn.classList.toggle("hidden")
+    })
+    
+
+    return btn
+}
+
 
 renderArticles(articleArray)
