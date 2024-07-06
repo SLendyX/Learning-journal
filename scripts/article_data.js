f9c1e5504f8fc15adf306dd2f2a0896ee6709a88
SLendyX <slendyx2002@gmail.com>
2024-07-06 15:26:06 +0300

    fixed some declarations

--- scripts/article_data.js
+++ scripts/article_data.js
@@ -5,42 +5,48 @@ export const articleArray =
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "1",
     date: "July 2, 2024",
-    link: "./blog1.html"
+    link: "./blog1.html",
+    alt: "a laptop sitting on a table next to a cactus"
 }, 
 {
     title: "Blog two",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "2",
     date: "July 3, 2024",
-    link: "./blog2.html"
+    link: "./blog2.html",
+    alt: "seomeone holding out a lightbulb"
 }, 
 {
     title: "Blog three",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "3",
     date: "July 4, 2024",
-    link: "#"
+    link: "#",
+    alt: "a laptop with text on it that reads 'I design and develop experiences taht make people's lives simple'"
 }, 
 {
     title: "Blog four",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "4",
     date: "July 5, 2024",
-    link: "#"
+    link: "#",
+    alt: "a gradiend purple mixed with the night sky"
 }, 
 {
     title: "Blog five",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "5",
     date: "July 6, 2024",
-    link: "#"
+    link: "#",
+    alt: "a blonde girl standing on the beach in front of the sun rising"
 }, 
 {
     title: "Blog six",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "6",
     date: "July 7, 2024",
-    link: "#"
+    link: "#",
+    alt: "a picture of some code with the react logo next to it"
 },
 //copy of the first half
 {
@@ -48,41 +54,47 @@ export const articleArray =
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "1",
     date: "July 2, 2024",
-    link: "./blog1.html"
+    link: "./blog1.html",
+    alt: "a laptop sitting on a table next to a cactus"
 }, 
 {
     title: "Blog two",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "2",
     date: "July 3, 2024",
-    link: "./blog2.html"
+    link: "./blog2.html",
+    alt: "seomeone holding out a lightbulb"
 }, 
 {
     title: "Blog three",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "3",
     date: "July 4, 2024",
-    link: "#"
+    link: "#",
+    alt: "a laptop with text on it that reads 'I design and develop experiences taht make people's lives simple'"
 }, 
 {
     title: "Blog four",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "4",
     date: "July 5, 2024",
-    link: "#"
+    link: "#",
+    alt: "a gradiend purple mixed with the night sky"
 }, 
 {
     title: "Blog five",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "5",
     date: "July 6, 2024",
-    link: "#"
+    link: "#",
+    alt: "a blonde girl standing on the beach in front of the sun rising"
 }, 
 {
     title: "Blog six",
     content: "I am exicted to go through all the courses and all the projects and prove to myself that can I achieve great feats and as such that I am worthy to conquer the world through my true will of power and determination.",
     image: "6",
     date: "July 7, 2024",
-    link: "#"
+    link: "#",
+    alt: "a picture of some code with the react logo next to it"
 } 
 ]
